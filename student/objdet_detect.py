6f5bd52e519208d21cafdba689c00ed0c2ee8074
enescingoz <enescngz0333@gmail.com>
2022-08-21 21:46:01 +0300

    feat: step-3 completed
    step-3 completed but getting no such file error. question submitted to udacity forum about this problem.

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -24,7 +24,8 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 # model-related
 from tools.objdet_models.resnet.models import fpn_resnet
-from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
@@ -61,7 +62,51 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        configs.saved_fn = 'fpn_resnet_18'
+        configs.arch = 'fpn_resnet'
+        configs.model_path = os.path.join(parent_path, "tools", "objdet_models", "resnet")
+        configs.pretrained_path = os.path.join(configs.model_path, "pretrained", "fpn_resnet_18_epoch_300.pth")
+        configs.pretrained_filename = os.path.join(configs.model_path, "pretrained", "fpn_resnet_18_epoch_300.pth")
+        configs.use_giou_loss = False
+        configs.num_samples = None
+        configs.num_workers = 4
+        configs.batch_size = 4
+        configs.gpu_idx = 0
+        configs.no_cuda = True
+        configs.K = 50
+        configs.conf_thresh = 0.5
+        configs.output_format = 'image'
+        configs.output_video_fn = 'out_fpn_resnet_18'
+        configs.output_width = 608
+        configs.img_size = 608
+        
+        configs.pin_memory = True
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
 
+        configs.imagenet_pretrained = False
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+
+        configs.heads = {
+            'hm_cen': configs.num_classes,
+            'cen_offset': configs.num_center_offset,
+            'direction': configs.num_direction,
+            'z_coor': configs.num_z,
+            'dim': configs.num_dim
+        }
+
+        configs.num_input_features = 4
+        
+        
+    
+    
         #######
         ####### ID_S3_EX1-3 END #######     
 
@@ -118,7 +163,10 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
+        num_layers = 18
+        model = fpn_resnet.get_pose_net(num_layers = num_layers, heads = configs.heads, head_conv= configs.head_conv, imagenet_pretrained = configs.imagenet_pretrained)
 
+        
         #######
         ####### ID_S3_EX1-4 END #######     
     
@@ -164,9 +212,15 @@ def detect_objects(input_bev_maps, model, configs):
         elif 'fpn_resnet' in configs.arch:
             # decode output and perform post-processing
             
+
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
+            outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
+            outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
+            detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'], outputs['dim'], K=configs.K)
+            detections = detections.cpu().numpy().astype(np.float32)
+            detections = post_processing(detections, configs)
 
             #######
             ####### ID_S3_EX1-5 END #######     
@@ -180,15 +234,27 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
-
+    if len(detections) > 0:
+        
         ## step 2 : loop over all detections
+        for detection in detections:
+            """
+            _id, _x, _y, _z, _h, _w, _l, yaw = detection
         
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
+            x = _y / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+            y = (_x / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) - (configs.lim_y[1] - configs.lim_y[0]) / 2.0)
+            z = _z / (configs.lim_z[1] - configs.lim_z[0]) + configs.lim_z[0]
+            h = _h / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+            w = _w / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0])
+            l = _l / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+            """
+            
             ## step 4 : append the current object to the 'objects' array
+            objects.append(detection)
         
     #######
-    ####### ID_S3_EX2 START #######   
+    ####### ID_S3_EX2 END #######   
     
     return objects    
 
